334785c0e7d0d271e9dacc06aa940832e12cc758
25019946 <25019946@mahindra.com>
2023-12-18 21:01:47 +0530

    ..

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -9,6 +9,7 @@ import {
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import {  MatInputModule } from '@angular/material/input';
+import {  MatIconModule } from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -17,7 +18,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './kanban.component.html',
   styleUrls: ['./kanban.component.scss'],
   standalone: true,
-  imports: [CdkDropList, CdkDrag, CommonModule, MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule],
+  imports: [CdkDropList, CdkDrag, CommonModule, MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule],
 })
 export class KanbanComponent {
 
@@ -50,6 +51,12 @@ export class KanbanComponent {
     this.addTodo = false
     this.currenttodo  = {name:'', description : '', status:'0'};
 
+  }
+
+  removeItem(index:number, code : number){
+    if(code == 0) this.todo.splice(index,1)
+    if(code == 1) this.inProgress.splice(index,1)
+    if(code == 2) this.done.splice(index,1)
   }
 
 }
